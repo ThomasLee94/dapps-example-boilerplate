e787ef0fa9b709b7ecabfed341bd14ddf9af53f5
Thomas <thomaslee9474@gmail.com>
2019-06-23 14:24:13 -0700

    [update] cannot connect to Rinkeby using the Infura API

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require('./compile')
+require('dotenv').config();
+
+console.log(process.env)
+
+const provider = new HDWalletProvider(
+    process.env.MNEMONIC,
+    process.env.RINKEBY_TEST_INFURA_URL
+)
+
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log("Attempting to deploy from account", accounts[0])
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ["Hello"]})
+        .send({gas: '1000', from: accounts[0]})
+    
+    console.log("Contract deployed to", reult.options.address)
+};
+deploy()
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -29,4 +29,9 @@ describe('Inbox', () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, 'Hi there!');
   });
+  it('can change a message', async () => {
+    await inbox.methods.setMessage("Bye!").send({ from: accounts[0]})
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Bye!');
+  });
 });
